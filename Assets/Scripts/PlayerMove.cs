629169040b9d45893ad414b457cc30b92d26a8f0
Alexey Danilin <adm.minefun@yandex.ru>
2018-04-25 21:44:16 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    CharacterController charControl;
+    public float walkSpeed;
+    public float runSpeed;
+    public bool cameraShake;
+
+    private float speed;
+
+    void Awake()
+    {
+        speed = 0;
+        charControl = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+        }
+        else
+        {
+            speed = walkSpeed;
+        }
+
+
+        Vector3 moveDirSide = transform.right * horiz * speed;
+        Vector3 moveDirForward = transform.forward * vert * speed;
+
+        charControl.SimpleMove(moveDirSide);
+        charControl.SimpleMove(moveDirForward);
+        if(cameraShake && (horiz != 0 || vert != 0))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                GetComponentInChildren<Animator>().SetFloat("speed", 1.3f);
+            }
+            else
+            {
+                GetComponentInChildren<Animator>().SetFloat("speed", 1f);
+            }
+        }
+        else
+        {
+            GetComponentInChildren<Animator>().SetFloat("speed", 0f);
+        }
+
+    }
+}
